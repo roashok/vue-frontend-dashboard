492e4affd55817174baaf83a6a53652ffb7e008a
Roshan Ashok <roashok@icloud.com>
2024-12-15 06:36:36 +0400

    Login and Register call

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,42 @@
 <template>
   <form>
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    <input v-model="data.email" type="email" class="form-control" placeholder="Email" required>
+
+    <input v-model="data.password" type="password" class="form-control" placeholder="Password" required>
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
   </form>
 </template>
 
 <script>
-export default{
-    name:"LoginComponent"
+import {reactive} from 'vue';
+import {useRouter} from "vue-router";
+export default {
+  name: "LoginComponent",
+  setup() {
+    const data = reactive({
+      email: '',
+      password: ''
+    });
+    const router = useRouter();
+
+    const submit = async () => {
+      await fetch('http://localhost:8000/api/login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        credentials: 'include',
+        body: JSON.stringify(data)
+      });
+
+      await router.push('/');
+    }
+
+    return {
+      data,
+      submit
+    }
+  }
 }
 </script>
