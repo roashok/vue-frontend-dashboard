761a48c6d75e12b1d08a15edced4f13ef0f9ef54
Roshan Ashok <roashok@icloud.com>
2024-12-15 05:30:22 +0400

    navbar basic

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-      
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+  <a class="navbar-brand" href="#">Login</a>
+</nav>
 <main class="form-signin w-100 m-auto">
   <form>
     <div class="form-floating">
@@ -17,4 +19,29 @@
 </template>
 
 <style>
+html,
+body {
+  height: 100%;
+}
+
+.form-signin {
+  max-width: 330px;
+  padding: 1rem;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
 </style>
